b00709734cd64bb1b0168715d4fb0987f7ae891a
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2020-05-23 15:46:02 -0400

    Added scrolling words and sprites to tell where Bloons go

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+namespace SpriteKind {
+    export const DirectionDown = SpriteKind.create()
+    export const DirectionLeft = SpriteKind.create()
+    export const DirectionRight = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -171,6 +176,30 @@ namespace myTiles {
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 `
 }
+function ScreenSay (Text: string) {
+    OnScreenNarrator.right = scene.cameraLeft()
+    OnScreenNarrator.vx = 50
+    OnScreenNarrator.say(Text)
+}
+info.onCountdownEnd(function () {
+    if (Waiting) {
+        Waiting = false
+        timer.after(500, function () {
+            info.startCountdown(15 * Wave)
+            ScreenSay("Start of wave " + Wave)
+        })
+    } else {
+        Waiting = true
+        ScreenSay("End of wave " + Wave)
+        Wave += 1
+        timer.after(500, function () {
+            info.startCountdown(15)
+        })
+    }
+})
+let Wave = 0
+let Waiting = false
+let OnScreenNarrator: Sprite = null
 let Cursor = sprites.create(img`
 f f . . . . . . . 
 f 1 f . . . . . . 
@@ -188,11 +217,169 @@ f . . . f 1 f . .
 . . . . . . f . . 
 `, SpriteKind.Player)
 Cursor.setFlag(SpriteFlag.StayInScreen, true)
+Cursor.setFlag(SpriteFlag.ShowPhysics, true)
 controller.moveSprite(Cursor, 100, 100)
 scene.cameraFollowSprite(Cursor)
+OnScreenNarrator = sprites.create(img`
+. 
+`, SpriteKind.Player)
+OnScreenNarrator.setFlag(SpriteFlag.RelativeToCamera, true)
+let Bloons = sprites.allOfKind(SpriteKind.Enemy)
+let Downs = sprites.allOfKind(SpriteKind.DirectionDown)
+let Lefts = sprites.allOfKind(SpriteKind.DirectionLeft)
+let Rights = sprites.allOfKind(SpriteKind.DirectionRight)
+Rights.push(sprites.create(img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionRight))
+Rights[0].setPosition(36, 42)
+Downs.push(sprites.create(img`
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionDown))
+Downs[0].setPosition(232, 36)
+Lefts.push(sprites.create(img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionLeft))
+Lefts[0].setPosition(225, 88)
+Downs.push(sprites.create(img`
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionDown))
+Downs[1].setPosition(24, 88)
+Rights.push(sprites.create(img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionRight))
+Rights[1].setPosition(36, 188)
+Downs.push(sprites.create(img`
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionDown))
+Downs[2].setPosition(232, 180)
+Lefts.push(sprites.create(img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`, SpriteKind.DirectionLeft))
+Lefts[1].setPosition(225, 230)
 scene.setBackgroundColor(7)
 tiles.setTilemap(tiles.createTilemap(
-            hex`1000100012050401011401011301011201010101010502030303030303030303030308140106070707070707070707070702040113010101011201140101140101050401010a03030303030303030303030204120105020707070707070707070702040112050414010101130101010112050401010504010d0e0e0e0e0e0e0f01050413010504010c111111111111100105040114050413010101010101011401050412010502030303030303030303030204010106070707070707070707070702041401120101140101120101010113050413030303030303030303030303030204010707070707070707070707070707090101140101121401010101130101010112`,
+            hex`1000100012050401011401011301011201010101010502030303030303030303030308140106070707070707070707070702040113010101011201140101140101050401010a03030303030303030303030204120105020707070707070707070707090112050414010101130101010112010101010504010d0e0e0e0e0e0e0e0e0e0f13010504010c111111111111111111100114050413010101010101011401010112010502030303030303030303030308010106070707070707070707070702041401120101140101120101010113050413030303030303030303030303030204010707070707070707070707070707090101140101121401010101130101010112`,
             img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -214,3 +401,11 @@ tiles.setTilemap(tiles.createTilemap(
             [myTiles.tile0,myTiles.tile1,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath3,sprites.castle.tilePath9,sprites.castle.tilePath1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile7,myTiles.tile8,myTiles.tile9,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileGrass2],
             TileScale.Sixteen
         ))
+info.setLife(100)
+info.setScore(100)
+Waiting = true
+Wave = 1
+info.startCountdown(15)
+forever(function () {
+    OnScreenNarrator.y = scene.screenHeight() / 2 + scene.cameraTop()
+})
